36cf545e158f3c05da05cbfcb627d156d3a474c7
Upendra Singhai <eupendrajunk@gmail.com>
2019-07-02 16:59:47 +0530

    renamed to config

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-import request_headers as h
-address = 'https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B0756CYWWD/ref=sr_1_3?crid=24411Z9VDNZZN&keywords=bose+qc35+ii&qid=1562061017&s=gateway&sprefix=bose+qc%2Caps%2C377&sr=8-3'
-source = requests.get(address, headers=h.headers).text
+import config 
+source = requests.get(configaddress, headers=config.headers).text
 soup = BeautifulSoup(source, 'lxml')
 price = soup.find(
     "span", 
