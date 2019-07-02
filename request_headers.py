d7ad4a9e34cfd06a804aff61c3c3be8ecbfcfa94
Upendra Singhai <eupendrajunk@gmail.com>
2019-07-02 16:49:45 +0530

    amazon web scrapper

---
+++ request_headers.py
@@ -0,0 +1,10 @@
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding':	'gzip, deflate',
+    'Accept-Language':	'en-US,en;q=0.9,hi;q=0.8',
+    'Connection':	'keep-alive',
+    'Host':	'www.amazon.com',
+    'Referer':	'https://www.google.com/',
+    'Upgrade-Insecure-Requests':	'1',
+    'User-Agent':	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+}
