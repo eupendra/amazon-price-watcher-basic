d824bcdf0eed9d9a0a5caa64a2dbfd21ab581c0c
Upendra Singhai <eupendrajunk@gmail.com>
2019-07-02 17:36:52 +0530

    three columns in csv

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# get headers from http://www.xhaus.com/headers
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
     'Accept-Encoding':	'gzip, deflate',
@@ -8,3 +9,5 @@ headers = {
     'Upgrade-Insecure-Requests':	'1',
     'User-Agent':	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
 }
+
+address = 'https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B0756CYWWD/ref=sr_1_3?crid=24411Z9VDNZZN&keywords=bose+qc35+ii&qid=1562061017&s=gateway&sprefix=bose+qc%2Caps%2C377&sr=8-3'
--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 import config 
-source = requests.get(configaddress, headers=config.headers).text
-soup = BeautifulSoup(source, 'lxml')
-price = soup.find(
-    "span", 
-    {"id": "priceblock_ourprice"}
-    ).text
-print(f'The price is {price}')
+import csv
+from datetime import datetime as d
+def get_price(address):
+    source = requests.get(address, headers=config.headers).text
+    soup = BeautifulSoup(source, 'lxml')
+    price = soup.find(
+        "span", 
+        {"id": "priceblock_ourprice"}
+        ).text
+
+    # print(f'The price of Bose QC35 is {price}')
+    return price
+
+# print(get_price(config.address))
+def log_in_csv(data:list):
+    file = open("price_history.csv","a") # will append
+    writer= csv.writer(file)  
+    writer.writerow(data)
+    file.close 
+
+
+price = get_price(config.address)
+date_part = f"{d.now():%Y-%m-%d}"
+time_part = f"{d.now():%H:%S}"
+
+data = [date_part,time_part,price]
+
+log_in_csv(data)
+
+print('~'*50)
+print('Done!')
